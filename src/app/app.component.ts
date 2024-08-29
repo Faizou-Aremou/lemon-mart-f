769f56a8d3ec500d1782930a3decc2830143e871
Faïzou Aremou <faizouaremou@gmail.com>
2024-08-29 07:38:14 +0200

    add toolbar and landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
-
+import { RouterLink, RouterOutlet } from '@angular/router'
+import { MatToolbarModule } from '@angular/material/toolbar'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, MatToolbarModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
